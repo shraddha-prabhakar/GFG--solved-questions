fd7797019dc6804725bcf90a7cbfa1e135c52ef2
Shraddha Prabhakar <140002488+shraddha-prabhakar@users.noreply.github.com>
2025-03-24 23:12:25 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Find element at a given Index/find-element-at-a-given-index.java
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+// Initial Template for Java
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class Main {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine().trim());
+
+        while (t-- > 0) {
+            int key = Integer.parseInt(read.readLine().trim());
+            String line = read.readLine().trim();
+            String[] numsStr = line.split(" ");
+            int[] nums = new int[numsStr.length];
+            for (int i = 0; i < numsStr.length; i++) {
+                nums[i] = Integer.parseInt(numsStr[i]);
+            }
+
+            Solution obj = new Solution();
+            int res = obj.findElementAtIndex(key, nums);
+            System.out.println(res);
+        
+System.out.println("~");
+}
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+class Solution {
+    public static int findElementAtIndex(int key, int[] arr) {
+        //int v = 0;
+       // int size = arr.length()-1;
+        for(int i=0;i<arr.length; i++) {
+            if(key==i) {
+               return arr[i];
+            }
+        }
+        return -1;
+    }
+}
